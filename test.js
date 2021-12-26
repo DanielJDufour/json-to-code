8c8f17130bfb18e66c66876a050d8be6a5e7002d
DanielJDufour <daniel.j.dufour@gmail.com>
2021-12-25 20:23:24 -0500

    updated tests

--- test.js
+++ test.js
@@ -7,15 +7,24 @@ const { encode } = require("./json-to-code");
 
 const TEST_DATA_PATH = path.resolve(__dirname, "./test-data");
 
-const check = ({ debug_level, eq, filename, max_passes, uid }) => {
-  const data = require(path.resolve(TEST_DATA_PATH, filename));
+const check = ({ data, debug_level, eq, filename, max_passes, uid }) => {
+  data ??= require(path.resolve(TEST_DATA_PATH, filename));
   const result = encode({ data, debug_level, max_passes });
   const outpath = path.resolve(TEST_DATA_PATH, filename.replace(".json", "-" + uid + ".tmp.js"));
   fs.writeFileSync(outpath, result.code, "utf-8");
+  console.log("wrote " + outpath);
 
   const decompressed = require(outpath);
   eq(decompressed, data);
 };
+
+test("proj", ({ eq }) => {
+  check({ eq, filename: "proj.json", max_passes: 100, uid: "" });
+});
+
+test("foss4g", ({ eq }) => {
+  check({ eq, filename: "foss4g-2021-schedule.json", uid: 123 });
+});
 
 for (let max_passes = 1; max_passes < 6; max_passes++) {
   test(`array (${max_passes} passes)`, ({ eq }) => {
